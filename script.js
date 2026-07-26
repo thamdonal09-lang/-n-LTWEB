002c68af60b7efcaff7504ccc9109165c025e960
thamdonal09-lang <thamdonal09@gmail.com>
2026-07-26 12:09:52 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const addressbtn = document.querySelector('address-form')
-const addressclose = document.querySelector('address-close')
-console.log(addressclose)
-addressbtn.addEventListener("click", function (){
-    document.querySelector('.address-form').style.display ="flex"
-})
-addressclose.addEventListener("click", function () {
-    document.querySelector('.address-form').style.display = "none"
-})
